08ef1d7245b4badf45d6852a77fbe9ae90de6eaf
goldangeee <goldangeee@proton.me>
2024-03-14 22:48:07 +0900

    density

--- test.py
+++ test.py
@@ -11,46 +11,46 @@ with open("SIDO_MAP_2022_cp949.json", "r", encoding='cp949') as f:
 # 인구 밀도 데이터를 불러옵니다.
 pop_den = pd.read_csv("pop_den.csv", encoding='utf-8')
 
-# 지역별 웹사이트 정보를 추가합니다.
+# # 지역별 2022년 인구밀도 데이터
 for feature in geo_data['features']:
     if feature['properties']['CTP_KOR_NM'] == '서울특별시':
-        feature['properties']['website'] = '서울특별시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "서울특별시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '부산광역시':
-        feature['properties']['website'] = '부산광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "부산광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '대구광역시':
-        feature['properties']['website'] = '대구광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "대구광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '인천광역시':
-        feature['properties']['website'] = '인천광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "인천광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '광주광역시':
-        feature['properties']['website'] = '광주광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "광주광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '대전광역시':
-        feature['properties']['website'] = '대전광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "대전광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '울산광역시':
-        feature['properties']['website'] = '울산광역시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "울산광역시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '세종특별자치시':
-        feature['properties']['website'] = '세종특별자치시의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "세종특별자치시",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '경기도':
-        feature['properties']['website'] = '경기도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "경기도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '강원도':
-        feature['properties']['website'] = '강원도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "강원도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '충청북도':
-        feature['properties']['website'] = '충청북도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "충청북도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '충청남도':
-        feature['properties']['website'] = '충청남도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "충청남도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '전라북도':
-        feature['properties']['website'] = '전라북도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "전라북도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '전라남도':
-        feature['properties']['website'] = '전라남도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "전라남도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '경상북도':
-        feature['properties']['website'] = '경상북도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "경상북도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '경상남도':
-        feature['properties']['website'] = '경상남도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "경상남도",'2022'].values[0])
     elif feature['properties']['CTP_KOR_NM'] == '제주특별자치도':
-        feature['properties']['website'] = '제주특별자치도의 인구밀도(예정)'
+        feature['properties']['website'] = str(pop_den.loc[pop_den["행정구역별"] == "제주특별자치도",'2022'].values[0])
 
 
 st.title("My ideal region")
-st.write("지역을 클릭 인구밀도를 확인해주세요")
+st.write("지역을 클릭해서 지표를 확인해주세요")
 
 # 지도 객체를 생성합니다.
 m = folium.Map(location=[36, 127], zoom_start=6)
