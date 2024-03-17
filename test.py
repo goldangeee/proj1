138ba8b3862b945a09b03b83f3f201cbd3de5b26
goldangeee <goldangeee@proton.me>
2024-03-17 23:07:53 +0900

    3/17

--- test.py
+++ test.py
@@ -15,14 +15,14 @@ with open("SIDO_MAP_2022_cp949.json", "r", encoding='cp949') as f:
 
 ##################################################################
     
-# 인구 밀도 데이터를 불러옵니다.
+# 인구 밀도 데이터
 pop_den = pd.read_csv("pop_den.csv", encoding='utf-8') # 명/㎢
 pop_den_df = pd.DataFrame(pop_den)
 filtered_pop_den_df = pop_den_df[['행정구역별', '2022']]
 
 # "전국" 행을 제외
 new_pop_den_df = filtered_pop_den_df.drop(filtered_pop_den_df[filtered_pop_den_df['행정구역별'] == '전국'].index)
-new_pop_den_df['rank'] = new_pop_den_df['2022'].rank(method='min').astype(int) # "rank" 열 생성과 순위 추가
+new_pop_den_df['rank'] = new_pop_den_df['2022'].rank(method='min').astype(int) # 데이터프레임에 "rank" 열 생성과 순위 추가
 
 ###################################################################################################################
 
@@ -37,7 +37,8 @@ filtered_pm25_df = pm25_df[(pm25_df['구분(1)'] == pm25_df['구분(2)']) | (pm2
 res_pm25_df = filtered_pm25_df[['구분(1)', '구분(2)', '2023.07']]
 
 new_pm25_df = res_pm25_df[['구분(1)', '2023.07']]
-new_pm25_df['rank'] = new_pm25_df['2023.07'].rank(method='min').astype(int) # "rank" 열 생성과 순위 추가
+new_pm25_df['rank'] = new_pm25_df['2023.07'].rank(method='min').astype(int) 
+# 데이터프레임에 "rank" 열 생성과 순위 추가
 
 # 에러 해결을 위해 안전한 숫자로 변환
 res_pm25_df['2023.07'] = pd.to_numeric(res_pm25_df['2023.07'], errors='coerce')
@@ -45,15 +46,15 @@ res_pm25_df['2023.07'] = pd.to_numeric(res_pm25_df['2023.07'], errors='coerce')
 ###################################################################################
 # 사이드바
 
-# 세션 상태에 'init_state' 속성이 없으면 False로 초기화합니다.
+# 세션 상태에 'init_state' 속성이 없으면 False로 초기화
 if "init_state" not in st.session_state:
     st.session_state.init_state = False
 
-# 세션 상태에 'show_density' 속성이 없으면 False로 초기화합니다.
+# 세션 상태에 'show_density' 속성이 없으면 False로 초기화
 if "show_density" not in st.session_state:
     st.session_state.show_density = False
 
-# 세션 상태에 'show_pm' 속성이 없으면 False로 초기화합니다.
+# 세션 상태에 'show_pm' 속성이 없으면 False로 초기화
 if "show_pm" not in st.session_state:
     st.session_state.show_pm = False
     
@@ -78,19 +79,25 @@ if show_pm:
     st.session_state.show_density = False
     st.session_state.show_pm = True    
 
+#########################################################
+
 # 지도 객체를 생성합니다.
 m = folium.Map(location=[36, 128], zoom_start=6.5)
+
+#############################################################
 
 # GeoJSON 레이어를 지도에 추가
 folium.GeoJson(
     geo_data,
-    name='geojson',
-    style_function=lambda x: {'fillColor': '#ffffff00'},
-    highlight_function=lambda x: {'weight':3, 'color':'blue'},
-    tooltip=folium.GeoJsonTooltip(fields=['CTP_KOR_NM']),            
+    name = 'geojson',
+    style_function = lambda x: {'fillColor': '#ffffff00'},
+    highlight_function = lambda x: {'weight':3, 'color':'blue'},
+    tooltip=folium.GeoJsonTooltip(fields=['CTP_KOR_NM']),              
 ).add_to(m)
 
-# 인구 밀도 버튼을 누르면, Choropleth 맵을 생성합니다.
+##################################################################################
+
+# 버튼을 누르면, Choropleth 맵을 생성
 if st.session_state.show_density:
     folium.Choropleth(
         geo_data=geo_data,
@@ -116,5 +123,5 @@ elif st.session_state.show_pm:
         legend_name='(단위: μg/m³)'
     ).add_to(m)
 
-# 지도를 표시합니다.
+# 지도를 표시
 st_folium(m, width=700, height=600)
